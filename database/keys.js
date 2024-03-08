7e0169ea3bd8a5df964a8db41c30bf59a6d17768
Britoshky <britoshky@gmail.com>
2024-03-07 21:13:28 -0300

    mongodb cambiado

--- database/keys.js
+++ database/keys.js
@@ -1,14 +1,10 @@
 require('dotenv').config();
 
-const DB_HOST = process.env.DB_HOST;
-const DB_USER = process.env.DB_USER;
-const DB_PASS = process.env.DB_PASS;
-const DB_NAME = process.env.DB_NAME;
+const HOST = process.env.DB_HOST;
 
 
-const host = DB_USER+":"+DB_PASS+"@"+DB_HOST+"/"+DB_NAME;
 module.exports = {
   database: {
-    URI: "mongodb+srv://"+host,
+    URI: HOST,
   }
 };
