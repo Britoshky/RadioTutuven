03a00ac390fbf9a4a25cb304ce112cc884d5dec3
Britoshky <britoshky@gmail.com>
2024-03-19 23:36:26 -0300

    chat

--- public/js/script.js
+++ public/js/script.js
@@ -1496,31 +1496,28 @@ socket.onmessage = function(event) {
 };
 
 
-function displayMessage(message, formattedTimestamp) {
-    const chatMessages = document.getElementById('chat-messages');
-    const messageItem = document.createElement('li');
-    messageItem.classList.add('message-item');
-    messageItem.innerHTML = `
-        <div class="message-content">${message}</div>
-        <div class="message-time">${formattedTimestamp}</div>
-    `;
-    chatMessages.appendChild(messageItem);
+function displayMessage(message) {
+	const chatMessages = document.getElementById('chat-messages');
+	const messageItem = document.createElement('li');
   
-    // Desplazar el contenedor de mensajes al final para mostrar el mensaje recién agregado
-    chatMessages.scrollTop = chatMessages.scrollHeight;
-}
-
+	// Obtener la hora actual y formatearla
+	const formattedTimestamp = moment().format("h:mm a");
+  
+	// Concatenar el mensaje y la hora formateada
+	messageItem.textContent = `${message}`;
+	
+	chatMessages.appendChild(messageItem);
+  }
   
   
 
 
-  document.getElementById('form').onsubmit = function (e) {
-    e.preventDefault(); // Evita que el formulario se envíe normalmente
-    let message = document.getElementById('input').value;
-    socket.send(message); // Envía el mensaje al servidor a través de WebSockets
-    document.getElementById('input').value = ''; // Limpia el campo de entrada
+document.getElementById('form').onsubmit = function (e) {
+	e.preventDefault();
+	let message = document.getElementById('input').value;
+	socket.send(message);
+	document.getElementById('input').value = '';
 };
-
 
 // Esperar a que se cargue completamente la página
 window.onload = function() {
