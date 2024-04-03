4923f14bd187a1c7791335b31ddfb470422c4e68
Britoshky <britoshky@gmail.com>
2024-04-03 18:33:53 -0300

    historias

--- routes/historias.js
+++ routes/historias.js
@@ -2,7 +2,15 @@ const express = require("express");
 const router = express.Router();
 
 router.get('/historias', function (req, res){
-    res.render('historias');
+    res.render('historias/faro_carranza_chanco');
+});
+
+router.get('/faro_carranza_chanco', function (req, res){
+    res.render('historias/faro_carranza_chanco');
+});
+
+router.get('/tranque_tutuven', function (req, res){
+    res.render('historias/tranque_tutuven');
 });
 
 module.exports = router;
