3d90498cf78e264e3277d479e314565acc852bf2
Héctor Brito <britoshky@gmail.com>
2024-07-17 20:21:27 -0400

    historias agregadas

--- routes/historias.js
+++ routes/historias.js
@@ -1,6 +1,27 @@
 const express = require("express");
 const router = express.Router();
 
+// COMUNAS
+
+router.get('/cauquenes', function (req, res){
+    res.render('historias/cauquenes');
+});
+
+router.get('/chanco', function (req, res){
+    res.render('historias/chanco');
+});
+
+router.get('/pelluhue', function (req, res){
+    res.render('historias/pelluhue');
+});
+
+router.get('/curanipe', function (req, res){
+    res.render('historias/curanipe');
+});
+
+
+// HISTORIAS
+
 router.get('/historias', function (req, res){
     res.render('historias/faro_carranza_chanco');
 });
@@ -13,17 +34,12 @@ router.get('/tranque_tutuven', function (req, res){
     res.render('historias/tranque_tutuven');
 });
 
-router.get('/cauquenes', function (req, res){
-    res.render('historias/cauquenes');
-});
-
-router.get('/chanco', function (req, res){
-    res.render('historias/chanco');
-});
-
 router.get('/cienagas-de-name', function (req, res){
     res.render('historias/cienagas-de-name');
 });
+
+
+// RESERVAS NACIONALES
 
 router.get('/reserva-nacional-federico-albert', function (req, res){
     res.render('historias/reserva-nacional-federico-albert');
@@ -32,5 +48,10 @@ router.get('/reserva-nacional-federico-albert', function (req, res){
 router.get('/reserva-nacional-los-ruiles', function (req, res){
     res.render('historias/reserva-nacional-los-ruiles');
 });
+
+
+
+
+
 
 module.exports = router;
