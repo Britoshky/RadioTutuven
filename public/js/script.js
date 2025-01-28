62990bb8c588a82f20438041c463dbbef6542348
Héctor Brito <britoshky@gmail.com>
2025-01-27 23:46:30 -0300

    botón de play cambiado de color

--- public/js/script.js
+++ public/js/script.js
@@ -1371,32 +1371,7 @@ $document.ready(function () {
 
 });
 
-const playBtn = document.getElementById("playBtn");
-const link = new Audio();
-let isPlaying = false;
 
-if (playBtn == null) {
-	console.log("playBtn es nulo");
-} else {
-	playBtn.addEventListener("click", async () => {
-		if (isPlaying) {
-			link.pause();
-			playBtn.innerHTML = '<div class="animated-text">ESCUCHA AQUÍ</div>';
-		} else {
-			playBtn.innerHTML = '<div class="spinner-border spinner-border-sm text-primary" role="status"><span class="visually-hidden">Loading...</span></div>  Cargando'; // Agrega "bi-spin" para la animación de rotación
-			link.src = "https://stream.chanquinafm.cl/radiotutuven";
-
-			// Elimina la animación de rotación cuando se ha cargado el audio
-			link.addEventListener("loadeddata", () => {
-				playBtn.innerHTML = '<i class="fa-solid fa-pause"></i> <div class="animated-text"> PAUSA</div>';
-				playBtn.classList.remove("bi-spin");
-			});
-
-			await link.play();
-		}
-		isPlaying = !isPlaying;
-	});
-}
 
 
 
